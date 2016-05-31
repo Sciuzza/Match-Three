19335a04e5d00045cea23218a755b17f103537b7
dbga_28 <dbga_28@dbga.local>
2016-05-31 16:34:38 +0200

    Coding cells behavior script

--- Assets/Scripts/Cell_S.cs
+++ Assets/Scripts/Cell_S.cs
@@ -1,46 +1,15 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-namespace MatchThree
-{
-    public class Cell_S : MonoBehaviour
-    {
-        public SpriteRenderer sr;
-        // define the enumerator for cells color
-        public enum block { red, blue, green, cyan, magenta };
+public class Cell_S : MonoBehaviour {
 
-        // get and initialize sprite renderer componet
-        void Awake()
-        {
-            sr = GetComponent<SpriteRenderer>();
-        }
-
-        // Use this for initialization
-        void Start()
-        {
-            // call the enumerator and get randomize the starting color
-            switch ((block)(Random.Range(0, 5)))
-            {
-                case block.red:
-                    sr.color = Color.red;
-                    break;
-
-                case block.blue:
-                    sr.color = Color.blue;
-                    break;
-
-                case block.green:
-                    sr.color = Color.green;
-                    break;
-
-                case block.cyan:
-                    sr.color = Color.cyan;
-                    break;
-
-                case block.magenta:
-                    sr.color = Color.magenta;
-                    break;
-            }
-        }
-    }
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
 }
