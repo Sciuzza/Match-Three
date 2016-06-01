419a7c18fbc8502b43dd6183821affe627b204b5
Cristiano <Juza9164@gmail.com>
2016-06-01 07:50:36 +0200

    Grid Generation and pseudo code written in Cell Script to prepare teamwork

--- Assets/Scripts/Cell_S.cs
+++ Assets/Scripts/Cell_S.cs
@@ -8,7 +8,7 @@ namespace MatchThree
         public SpriteRenderer sr;
         // define the enumerator for cells color
         public enum block { red, blue, green, cyan, magenta };
-
+        
         // get and initialize sprite renderer componet
         void Awake()
         {
@@ -41,6 +41,27 @@ namespace MatchThree
                     sr.color = Color.magenta;
                     break;
             }
+
+
+        }
+
+        void OnMouseUp()
+        {
+
+          //if current game phase is set to Animation will appear a debug message saying "animation in progress..." and click will have no effects
+
+          //else if current game phase is set to sel_source the player will be able to select 1 block
+
+          //else if current game phase is set to sel_dest the player will be able to select an adiacent block to the first one selected in the previous game phase
+            
+            // if the selected block is not adiacent to the previous one will appear a message on the screen indicating that the selection is forbidden
+            
+            // else if the selected block is adiacent but the swap will not produce any combo will appear a message on the screen that the selection is forbidden
+            
+            // else the game phase will be set to animation (to prevent any click during the animation), blocks will be swapped, 
+            // every tris will be destroyed updating the current score that will be visible on scene and in the end new blocks will appears to the top and will fall down til they reach their position
+            // when the last action will be completed the game phase will be set again to sel_source
+
         }
     }
 }
--- Assets/Scripts/MThree_S.cs
+++ Assets/Scripts/MThree_S.cs
@@ -1,15 +1,36 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class MThree_S : MonoBehaviour {
+namespace MatchThree
+{
+    public class MThree_S : MonoBehaviour
+    {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+        GameObject cell_linking_init;
+        GameObject[,] cell_pointers = new GameObject[8, 10];
+
+        public enum game_phases { animation, sel_source, sel_dest};
+        public game_phases current_gp = game_phases.sel_source;
+
+        void Awake()
+        {
+            cell_linking_init = Resources.Load<GameObject>("Cell");
+
+            for (int i = 0; i < cell_pointers.GetLength(0); i++)
+            {
+                for (int j = 0; j < cell_pointers.GetLength(1); j++)
+                {
+                    cell_linking_init = Instantiate(cell_linking_init);
+                    cell_linking_init.transform.position = new Vector2(j - 4.5f, i - 3.5f);
+                    cell_linking_init.name = "Cell" + i + "," + j;
+                }
+            }
+
+            
+
+        }
+
+
+
+    }
 }
