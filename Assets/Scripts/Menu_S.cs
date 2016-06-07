071d0312a84898b4b948ebee095c39559c51a766
dbga_28 <dbga_28@dbga.local>
2016-06-07 17:01:55 +0200

    Firts try to implement Meny

---
+++ Assets/Scripts/Menu_S.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+namespace MatchThree
+{
+    public class Menu_S : MonoBehaviour
+    {
+
+        public SpriteRenderer sr;
+
+        void Awake() //Vengono chiamati prima degli start, da inserire per inizializzare le classi. E' simile ad un costruttore.
+        {
+            sr = GetComponent<SpriteRenderer>();
+        }
+
+        void OnMouseUP()
+        {
+            Debug.Log("Relase");
+            Scene gameScene = SceneManager.GetSceneByName("Gameplay");
+            SceneManager.SetActiveScene(gameScene);
+            sr.color = new Color(1, 0, 0, 1);
+        }
+
+        /*void OnMouseOver()
+        {
+            Debug.Log("Test");
+            sr.color = new Color(1, 0, 0, 1);
+        }*/
+
+    }
+}
