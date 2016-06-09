e1afd5bd2637dcea54b6b18d523e3bb241ce73ec
dbga_28 <dbga_28@dbga.local>
2016-06-09 17:13:26 +0200

    Add game name Add game font Add credit screen Improved main menu

---
+++ Assets/Scripts/UI_S.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace MatchThree
+{
+    public class UI_S : MonoBehaviour {
+
+        // Scene selector
+        public void StartGame()
+        {
+            SceneManager.LoadScene("Cri_Scene");
+        }
+
+        public void Credits()
+        {
+            SceneManager.LoadScene("Credits");
+        }
+
+        public void BackToMenu()
+        {
+            SceneManager.LoadScene("Menu");
+        }
+
+        // Quit game script
+        public void QuitGame()
+        {
+            Application.Quit();
+        }
+    }
+}
