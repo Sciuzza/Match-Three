110ccf7f7e5a2a078326cd5f4f7db7bb9ab8f33d
dbga_28 <dbga_28@dbga.local>
2016-06-09 09:44:32 +0200

    Add UI start UI

---
+++ Assets/Scripts/StartGame_S.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class StartGame_S : MonoBehaviour {
+
+	public void StartGame()
+    {
+        SceneManager.LoadScene("Cri_Scene");
+    }
+	
+}
